9162bf6549fe0ad4137cf19e4c1b300786d8a3c7
Dariusz Zając <darekdmc@gmail.com>
2023-08-17 15:59:08 +0200

    Tests cd

---
+++ ChallengeApp/ChallengeApp.Tests/TypeTests.cs
@@ -0,0 +1,20 @@
+﻿namespace ChallengeApp.Tests
+{
+    internal class TypeTests
+    {
+        [Test]
+        public void When()
+        {
+            // arrange
+            
+
+            // act
+            
+
+            // assert
+            
+        }
+
+    }
+}
+
