e3b54c591c49dc008e720410aed1d481f2b14419
Dariusz Zając <darekdmc@gmail.com>
2023-08-18 16:19:35 +0200

    Zadanie domowe dzień 8 - l1d8

--- ChallengeApp/ChallengeApp.Tests/TypeTests.cs
+++ ChallengeApp/ChallengeApp.Tests/TypeTests.cs
@@ -5,50 +5,46 @@ namespace ChallengeApp.Tests
     public class TypeTests
     {
         [Test]
-        public void TEST()
-        {
-            // arrange
-            string number1 = "1";
-            string number2 = "2";
-
-
+        public void IntValueType_test()
+        {   // arrange
+            int number1 = 1;
+            int number2 = 2;
             // act
-            
-
             // assert
             Assert.AreNotEqual(number1, number2);
         }
-        private User GetUser(string name) 
-        {
-            return new User(name);
-        }
-    }
-}
-
-/*
- namespace ChallengeApp.Tests
-{
-    public class TypeTests
-    {
         [Test]
-        public void TEST()
+        public void FloatValueType_test()
+        {   // arrange
+            float number3 = 2.5f;
+            float number4 = 2.5f;
+            // act
+            var floatsum = number3 + number4;
+            // assert
+            Assert.AreEqual(5, floatsum);
+        }
+        [Test]
+        public void StringHybridType_test()
+        {   // arrange
+            string number5 = "1";
+            string number6 = "2";
+            // act
+            // assert
+            Assert.AreNotEqual(number5, number6);
+        }
+        [Test]
+        public void StringReferenceType_test()
         {
             // arrange
             var user1 = GetUser("Darek");
             var user2 = GetUser("Darek");
-
-
             // act
-            
-
             // assert
             Assert.AreEqual(user1.Login, user2.Login);
         }
-        private User GetUser(string name) 
+        private User GetUser(string name)
         {
             return new User(name);
         }
     }
 }
-
- */
